29278d7e5d49ee1df548b75a7d3cc9f63160d378
maximus-sallam <maximus.sallam@gmail.com>
2019-08-20 19:48:53 -0400

    lined up input values with squares values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
