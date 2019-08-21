6e46dddbaec584b7e52197a9126098edf7d00e84
maximus-sallam <maximus.sallam@gmail.com>
2019-08-21 18:59:29 -0400

    completed example 15-1 on page 331

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_cube_values = list(range(1, 5001))
+y_cube_values = [x**3 for x in x_cube_values]
 
-plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,
-            edgecolor='none', s=40)
+x_square_values = list(range(1, 5001))
+y_square_values = [x**2 for x in x_square_values]
+
+plt.scatter(x_cube_values, y_cube_values, c=(0, 0, 1), edgecolor='none', s=40)
+plt.scatter(x_square_values, y_square_values, c=(1, 0, 0), edgecolor='none', s=40)
 
 # Set chart title and label axes.
-plt.title("Square Numbers", fontsize=24)
+plt.title("Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+plt.ylabel("Numbers of Value", fontsize=14)
 
 # Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
 
 # Set the range for each axis.
-plt.axis([0, 1100, 0, 1100000])
+plt.axis([0, 5000, 0, 12500000])
 
 plt.show()
 # plt.savefig('squares_plot.png', bbox_inches='tight')
