5e5464aa2ca46009d2078d16ca6a264f8bb944c0
maximus-sallam <maximus.sallam@gmail.com>
2019-08-21 23:53:16 -0400

    plotted starting and endpoints as different colors and larger plots, and changed color gradient to viridis

--- rw_visual.py
+++ rw_visual.py
@@ -9,8 +9,14 @@ while True:
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds,
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.viridis,
                 edgecolors='none', s=15)
+
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c=(0, 0, 0), edgecolors='none', s=30)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c=(0, 0, 0),
+                edgecolors='none', s=30)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
