5963a9fd4dd28a2482439eee87540d4a4176656f
maximus-sallam <maximus.sallam@gmail.com>
2019-08-22 20:53:30 -0400

    fixed typo: missing comma in list

--- different_dice.py
+++ different_dice.py
@@ -23,7 +23,7 @@ for value in range(2, max_result+1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling a D6 and a D10 1,000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
                  '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
