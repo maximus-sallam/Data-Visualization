be972cd9b8b9f8552e397ed59511eb5e22e8f219
maximus-sallam <maximus.sallam@gmail.com>
2019-08-22 20:48:04 -0400

    updated what the svg file will be called

--- different_dice.py
+++ different_dice.py
@@ -29,4 +29,4 @@ hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
 hist.add('D6 + D10', frequencies)
-hist.render_to_file('dice_visual.svg')
+hist.render_to_file('different_dice.svg')
