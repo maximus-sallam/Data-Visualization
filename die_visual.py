292f290233746e44087952fabb613b630fb4a1ad
maximus-sallam <maximus.sallam@gmail.com>
2019-08-22 21:06:38 -0400

    completed example 15-6 on page 346

--- die_visual.py
+++ die_visual.py
@@ -13,17 +13,17 @@ for roll_num in range(1000):
 
 # Analyze the results.
 frequencies = []
+x_labels = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
-
-print(frequencies)
+    x_labels.append(value)
 
 # Visualize the results.
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1,000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = x_labels
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
--- different_dice.py
+++ different_dice.py
@@ -14,17 +14,18 @@ for roll_num in range(50000):
 
 # Analyze the results.
 frequencies = []
+x_labels = []
 max_result = die_1.num_sides + die_2.num_sides
 for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+    x_labels.append(value)
 
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling a D6 and a D10 1,000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-                 '13', '14', '15', '16']
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = x_labels
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
